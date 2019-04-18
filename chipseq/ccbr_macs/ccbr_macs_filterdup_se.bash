ead4d9243343abd005b70060e0911ae694cb6d83
Vishal Koparde Ph.D <vishal.koparde@nih.gov>
2019-04-18 12:05:42 -0400

    nreads added to ccbr_macs

--- chipseq/ccbr_macs/ccbr_macs_filterdup_se.bash
+++ chipseq/ccbr_macs/ccbr_macs_filterdup_se.bash
@@ -18,6 +18,7 @@ awk -F"\t" -v OFS="\t" '{if ($2>0 && $3>0) {print}}' TmpTagAlign > TmpTagAlign2
 samtools view -@ $cpus -H $BAM|grep "^@SQ"|cut -f2,3|sed "s/SN://"|sed "s/LN://g" > GenomeFile
 awk -F"\t" -v OFS="\t" '{print $1,1,$2}' GenomeFile |sort -k1,1 -k2,2n > GenomeFileBed
 bedtools intersect -wa -f 1.0 -a TmpTagAlign2 -b GenomeFileBed > TagAlign
+cat TagAlign|wc -l |awk '{printf("%d\tAfter Deduplication\n",$1)}' > nreads.txt
 
 bedtools bedtobam -i TagAlign -g GenomeFile > OutBam
 
