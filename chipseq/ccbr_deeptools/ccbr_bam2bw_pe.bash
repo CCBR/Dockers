71f973bf743114eea029569d712e6d74efc5d41e
Vishal Koparde <vishal.koparde@nih.gov>
2019-08-22 08:14:55 -0400

    chipseq/ccbr_deeptools/ccbr_bam2bw_pe.bash edit to work for PE reads

--- chipseq/ccbr_deeptools/ccbr_bam2bw_pe.bash
+++ chipseq/ccbr_deeptools/ccbr_bam2bw_pe.bash
@@ -16,13 +16,6 @@ treatmentbw=`echo $TREATMENTBAM|sed "s/.bam/.bw/g"`
 inputbw=`echo $INPUTBAM|sed "s/.bam/.bw/g"`
 inputnormbw=`echo $TREATMENTBAM|sed "s/.bam/.inputnorm.bw/g"`
 
-treatmentextsize=`cat $TREATMENTPPQT|awk -F"\t" '{print $3}'|awk -F"," '{print $1}'`
-if [ $treatmentextsize -lt 0 ];then
-    treatmentextsize=`cat $TREATMENTPPQT|awk -F"\t" '{print $3}'|awk -F"," '{print $2}'`
-fi  
-if [ $treatmentextsize -lt 0 ];then
-    treatmentextsize="200"
-fi  
 
 treatmentsortedbam=`echo $TREATMENTBAM|sed "s/.bam/.sorted.bam/g"`
 inputsortedbam=`echo $INPUTBAM|sed "s/.bam/.sorted.bam/g"`
@@ -33,8 +26,8 @@ samtools sort -@ $NCPUS -o $inputsortedbam $INPUTBAM
 samtools index $treatmentsortedbam
 samtools index $inputsortedbam
 
-bamCoverage --bam $treatmentsortedbam -o $treatmentbw --binSize 25 --smoothLength 75 --ignoreForNormalization chrX chrY chrM --numberOfProcessors $NCPUS --normalizeUsing RPGC --effectiveGenomeSize $EFFECTIVEGENOMESIZE --extendReads $treatmentextsize
+bamCoverage --bam $treatmentsortedbam -o $treatmentbw --binSize 25 --smoothLength 75 --ignoreForNormalization chrX chrY chrM --numberOfProcessors $NCPUS --normalizeUsing RPGC --effectiveGenomeSize $EFFECTIVEGENOMESIZE --centerReads
 
-bamCoverage --bam $inputsortedbam -o $inputbw --binSize 25 --smoothLength 75 --ignoreForNormalization chrX chrY chrM --numberOfProcessors $NCPUS --normalizeUsing RPGC --effectiveGenomeSize $EFFECTIVEGENOMESIZE --extendReads 200
+bamCoverage --bam $inputsortedbam -o $inputbw --binSize 25 --smoothLength 75 --ignoreForNormalization chrX chrY chrM --numberOfProcessors $NCPUS --normalizeUsing RPGC --effectiveGenomeSize $EFFECTIVEGENOMESIZE --centerReads
 
 bigwigCompare --binSize 25 --outFileName $inputnormbw --outFileFormat 'bigwig' --bigwig1 $treatmentbw --bigwig2 $inputbw --operation 'subtract' --skipNonCoveredRegions --numberOfProcessors $NCPUS
