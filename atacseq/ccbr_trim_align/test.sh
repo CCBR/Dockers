15a623ec8e13ea0db83a264dffe9f22ffdcebaa8
Vishal Koparde <vishal.koparde@nih.gov>
2019-12-06 09:53:51 -0500

    adding db folder and multiqc version 1.8

--- atacseq/ccbr_trim_align/test.sh
+++ atacseq/ccbr_trim_align/test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-. /opt/conda/etc/profile.d/conda.sh
+. /opt2/conda/etc/profile.d/conda.sh
 conda activate python3
-python --version
-cutadapt --version
-idr -h
-macs2 --version
-echo $PATH
-python /opt/ccbr_bam_filter_by_mapq.py --help
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+#python --version
+#cutadapt --version
+#idr -h
+#macs2 --version
+#echo $PATH
+python /opt2/ccbr_bam_filter_by_mapq.py --help
+multiqc --version
 conda deactivate
-fastqc --version
+#fastqc --version
 
