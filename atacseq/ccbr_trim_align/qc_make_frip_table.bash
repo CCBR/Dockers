46ecd6e54db25270f0215ae81aaea07cf71986b4
Vishal N Koparde <vishal.koparde@nih.gov>
2020-06-09 17:55:45 -0400

    qc scripts merged into 1 file... transpose.sh added

--- atacseq/ccbr_trim_align/qc_make_frip_table.bash
+++
@@ -1,2 +0,0 @@
-SCRIPTSFOLDER=$1
-find . -name "*.frip" -not -name "*init*" -exec cat {} \; | python ${SCRIPTSFOLDER}/qc_get_frip_stats_table.py
--- atacseq/ccbr_trim_align/qc_make_peak_width_density.bash
+++
@@ -1,6 +0,0 @@
-SCRIPTSFOLDER=$1
-for f in `find . -name "*consensus*annotated"`;do 
-echo $f
-python ${SCRIPTSFOLDER}/qc_annotated2peakwidthdensity.py $f > ${f}.peak_width_density
-gzip -n $f
-done
