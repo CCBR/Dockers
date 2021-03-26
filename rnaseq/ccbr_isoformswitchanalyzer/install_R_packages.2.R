b4fccccd5a2273570205b97d95059fb7d5eb8882
kopardev <vishal.koparde@nih.gov>
2021-03-26 12:43:17 -0400

    isoformswithanalyzer

---
+++ rnaseq/ccbr_isoformswitchanalyzer/install_R_packages.2.R
@@ -0,0 +1,7 @@
+BiocManager::install("BSgenome.Mmusculus.UCSC.mm9")
+BiocManager::install("BSgenome.Mmusculus.UCSC.mm10")
+BiocManager::install("BSgenome.Mmusculus.UCSC.hg19")
+BiocManager::install("BSgenome.Mmusculus.UCSC.hg38")
+
+
+
---
+++ rnaseq/ccbr_isoformswitchanalyzer/install_R_packages.R
@@ -0,0 +1,11 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("IsoformSwitchAnalyzeR")
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
+BiocManager::install("BSgenome.Mmusculus.UCSC.mm9")
+BiocManager::install("BSgenome.Mmusculus.UCSC.mm10")
+
+
+
+
