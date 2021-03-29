0e180e36c2a66558b42ea3ea56f7356119cb490a
kopardev <vishal.koparde@nih.gov>
2021-03-29 10:27:55 -0400

    enrichplot

---
+++ atacseq/ccbr_enrichplot/install_R_packages_chipseeker.R
@@ -0,0 +1,10 @@
+BiocManager::install("GOSemSim")
+BiocManager::install("enrichplot")
+BiocManager::install("ChIPseeker")
+BiocManager::install("TxDb.Mmusculus.UCSC.mm9.knownGene")
+BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
+BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+BiocManager::install("TxDb.Hsapiens.UCSC.hg38.knownGene")
+BiocManager::install("org.Hs.eg.db")
+BiocManager::install("org.Mm.eg.db")
+
