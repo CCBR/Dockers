af7b77843b74372a017127f924a9d1106e82b10f
Vishal Koparde <vishal.koparde@nih.gov>
2022-06-28 19:45:10 +0000

    adding build.sh

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ "$#" != "2" ];then
+	echo "bash $0 requires 2 arguments!"
+	echo "argument1: path to Dockerfile"
+	echo "argument2: DockerHub tag"
+	exit
+fi
+
+dt=$(date +"%d%m%y%H%M%S")
+
+docker build \
+	-f $1 \
+	-t $2 \
+ 	. | \
+	tee docker_build_${dt}.log
+
+exitcode=$_
+
+if [ "$exitcode" == "0" ];then
+docker push $2
+fi
