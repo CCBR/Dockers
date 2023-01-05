ee67d5da3f43d09e59107c370e689c12abba3847
kopardev <vishal.koparde@nih.gov>
2023-01-05 10:51:27 -0500

    atacseq: adding support for mmul and btaurus

---
+++ atacseq/ccbr_atacseq/install_packages_v1.9.R
@@ -0,0 +1,4 @@
+BiocManager::install("TxDb.Btaurus.UCSC.bosTau9.refGene")
+BiocManager::install("TxDb.Mmulatta.UCSC.rheMac10.refGene")
+BiocManager::install("org.Mmu.eg.db")
+BiocManager::install("org.Bt.eg.db")
